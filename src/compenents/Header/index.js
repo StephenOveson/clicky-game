a69949d612541f3064ef3b5785730e052795058b
Spiritbr8ker <stephen.oveson@gmail.com>
2019-09-24 08:53:01 -0600

    Change of styling

--- src/compenents/Header/index.js
+++ src/compenents/Header/index.js
@@ -3,11 +3,9 @@ import React from 'react'
 const Header = (props) => {
     return (
     <div className="container-fluid bg-dark text-white">
-        <div className="row">
-            <div className="col mx-auto text-center">
+            <div className="col mx-auto text-center jumbotron bg-dark">
                 <h1>{props.game}</h1>
             </div>
-        </div>
     </div>
     )
 }
